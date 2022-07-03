6455a6b7db6c98fb7df13de001d0cab9146bb11a
Alexander Dyuzhev <dyuzhev@gmail.com>
2022-07-03 20:19:08 +0300

    Updated for #152

--- src/main/java/org/metanorma/utils/Util.java
+++ src/main/java/org/metanorma/utils/Util.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -233,27 +234,44 @@ public class Util {
         return outFilename;
     }
     
-    public static String getXMLFormat(String inputXmlFile) {
-        DocumentBuilderFactory fact = DocumentBuilderFactory.newInstance();
-        try {
-            DocumentBuilder builder = fact.newDocumentBuilder();
-            Document doc;
-            if (inputXmlFile.toLowerCase().startsWith("http") || inputXmlFile.toLowerCase().startsWith("www.")) {
-                doc = builder.parse(inputXmlFile);
-            } else {
-                doc = builder.parse(new FileInputStream(inputXmlFile));
+    public static String getInputFormat(String inputXmlFile, boolean... isIgnoreDTD) {
+        if (inputXmlFile.toLowerCase().endsWith(".docx")) {
+            return "docx";
+        } else {
+            DocumentBuilderFactory fact = DocumentBuilderFactory.newInstance();
+            try {
+                if (isIgnoreDTD.length > 0 && isIgnoreDTD[0]) {
+                    fact.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+                }
+                DocumentBuilder builder = fact.newDocumentBuilder();
+                Document doc;
+                
+                if (inputXmlFile.toLowerCase().startsWith("http") || inputXmlFile.toLowerCase().startsWith("www.")) {
+                    doc = builder.parse(inputXmlFile);
+                } else {
+                    doc = builder.parse(new FileInputStream(inputXmlFile));
+                }
+                
+                Node node = doc.getDocumentElement();
+                String root = node.getNodeName();
+                if (root.endsWith("-standard") || root.equals("metanorma-collection")) {
+                    return "metanorma";
+                } else if (root.equals("rfc"))  {
+                    return "rfc";
+                } else {
+                    return "sts";
+                }
+            } catch (FileNotFoundException ex) {
+                if (ex.toString().toLowerCase().contains(".dtd") && isIgnoreDTD.length == 0) {
+                    logger.log(Level.WARNING, "Can''t load DTD: {0}", ex.toString());
+                    return getInputFormat(inputXmlFile, true);
+                } else {
+                    logger.warning(ex.toString());
+                }
             }
-            Node node = doc.getDocumentElement();
-            String root = node.getNodeName();
-            if (root.endsWith("-standard") || root.equals("metanorma-collection")) {
-                return "metanorma";
-            } else if (root.equals("rfc"))  {
-                return "rfc";
-            } else {
-                return "sts";
+            catch (ParserConfigurationException | SAXException | IOException ex) {
+                logger.severe(ex.toString());
             }
-        } catch (ParserConfigurationException | SAXException | IOException ex) {
-            logger.severe(ex.toString());
         }
         return "";
     }
