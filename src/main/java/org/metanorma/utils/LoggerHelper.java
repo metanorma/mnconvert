1ce641555164852636bb6e35ebf73fa7065e09c9
Alexander Dyuzhev <dyuzhev@gmail.com>
2026-03-18 19:38:07 +0300

    LoggerHelper updated for metanorma/mnconvert-ruby#28

--- src/main/java/org/metanorma/utils/LoggerHelper.java
+++ src/main/java/org/metanorma/utils/LoggerHelper.java
@@ -15,7 +15,9 @@ public final class LoggerHelper {
     
     public static void setupLogger() {
         //System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tF %1$tT] [%4$s] %5$s%6$s%n");
-        System.setProperty("java.util.logging.SimpleFormatter.format", "%5$s%6$s%n");
+        // %5$: message (formatted log message)
+        // %6$: thrown (exception stack trace, if any)
+        System.setProperty("java.util.logging.SimpleFormatter.format", "[mnconvert] %5$s%6$s%n");
         //System.setProperty("handlers", "java.util.logging.ConsoleHandler");
     }
 }
